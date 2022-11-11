c10f0e008491b621f1d76ae8d17eb5007eb4d722
meteorafhd@gmail.com <meteorafhd@gmail.com>
2022-11-11 16:23:28 +0800

    HazelcastInstanceTest

---
+++ august/src/test/java/util/HazelcastInstanceTest.java
@@ -0,0 +1,29 @@
+package util;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import org.junit.Test;
+
+import java.util.concurrent.ConcurrentMap;
+
+public class HazelcastInstanceTest {
+    
+    @Test
+    public void demo( ) {
+        Config config = new Config();
+        HazelcastInstance h = Hazelcast.newHazelcastInstance(config);
+        ConcurrentMap<String, String> map = h.getMap("my-distributed-map");
+        map.put("key", "value");
+        System.out.println(map.get("key"));
+
+        //Concurrent Map methods
+        map.putIfAbsent("somekey", "somevalue");
+        map.replace("key", "value", "newvalue");
+        System.out.println(map.get("key"));
+        
+        h.shutdown();
+    }
+    
+    
+}
