7bfacf94c5404e227acea6cc798c78c365c02320
meteorafhd@gmail.com <meteorafhd@gmail.com>
2023-01-04 15:50:05 +0800

    add test

--- july/src/test/java/util/RedissonTest.java
+++ july/src/test/java/util/RedissonTest.java
@@ -2,17 +2,29 @@ package util;
 
 import com.fhdone.java2022.july.BaseTest;
 import com.fhdone.java2022.july.config.RedissonConfig;
-import org.junit.Test;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.RandomStringUtils;
+import org.apache.commons.lang.StringUtils;
+import org.junit.Assert;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.redisson.api.RMap;
 import org.redisson.api.RRingBuffer;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes= RedissonConfig.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class )  
+@Slf4j
 public class RedissonTest extends BaseTest {
 
     @Autowired
     private RedissonClient redissonClient;
+
+    public static final String CERT_MAP = "CERT_MAP";
 
     @Test
     public void testRing() throws Exception {
@@ -33,5 +45,52 @@ public class RedissonTest extends BaseTest {
 
 // buffer state is 3, 4, 5, 6
     }
+
+
+
+    @Test
+    @Order(1)
+    public void putKeysToRedisMap(){
+
+        RMap<String, String> rMap = redissonClient.getMap(CERT_MAP);
+        rMap.put("testKey", "testValue");
+
+        int i=0;
+        while (i<100_000){
+            String certNo = RandomStringUtils.random(18, false, true);
+            String name = RandomStringUtils.random(20, true, false);
+            rMap.put(certNo,name);
+
+            i++;
+            if(i%5000==0){
+                log.info("{},finished" , i );
+            }
+        }
+    }
+
+
+    @Test
+    @Order(2)
+    public void testQueryCert(){
+
+        RMap<String, String> rMap = redissonClient.getMap(CERT_MAP);
+        String testValue = rMap.get("testKey");
+        Assert.assertEquals("testValue",testValue);
+        
+        int i=0;
+        while (i<100_000) {
+            String certNo = RandomStringUtils.random(18, false, true);
+            String value = rMap.get(certNo);
+
+            if(StringUtils.isNotBlank(value)){
+                log.info("getter! {} ", value);
+            }
+            i++;
+            if(i%5000==0){
+                log.info("{},finished" , i );
+            }
+        }
+    }
+
 
 }
