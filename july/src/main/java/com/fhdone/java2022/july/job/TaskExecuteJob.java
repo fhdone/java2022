56926db191bc9e1347ad73a69824a9303cfa2ab6
meteorafhd@gmail.com <meteorafhd@gmail.com>
2023-01-12 17:56:18 +0800

    update CronExpressionTest

--- july/src/main/java/com/fhdone/java2022/july/job/TaskExecuteJob.java
+++ july/src/main/java/com/fhdone/java2022/july/job/TaskExecuteJob.java
@@ -1,33 +1,52 @@
 package com.fhdone.java2022.july.job;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.util.Random;
+import java.util.concurrent.*;
 
 @Component
 @Slf4j
 public class TaskExecuteJob {
 
+    private static final int JOB_RUN_ELASPE = 10;
+
+    private ThreadFactory namedThreadFactory = new ThreadFactoryBuilder()
+        .setNameFormat("TASK-EXECUTE-JOB-POOL-%d").build();
+
+    private Executor taskExecutor =  new ThreadPoolExecutor(2, 2,
+        0L, TimeUnit.MILLISECONDS,
+        new LinkedBlockingQueue<>(), namedThreadFactory);
+
+    private static final Random RANDOM = new Random();
+
     @PostConstruct
     public void taskExecuteRunner() {
         log.info("taskExecuteRunner start");
-        taskExecute();
+        taskExecutor.execute(new TaskRunner());
+        taskExecutor.execute(new TaskRunner());
     }
 
-    private void taskExecute() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (true) {
-                    try {
-                        Object obj = TaskQueryRunner.getJOB_QUEUE().take();
-                        log.info("get job: {}", obj.toString());
-                    } catch (Exception e) {
-                        log.error("taskExecute error:", e);
-                    }
+    class TaskRunner implements Runnable{
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    Object obj = TaskQueryRunner.getJOB_QUEUE().take();
+
+                    log.info("Start process job: {}", obj);
+                    TimeUnit.SECONDS.sleep(RANDOM.nextInt(TaskExecuteJob.JOB_RUN_ELASPE));
+                    log.info("End process job: {}", obj);
+
+                } catch (Exception e) {
+                    log.error("taskExecute error:", e);
                 }
             }
-        }).start();
+        }
     }
+
+
 }
--- july/src/main/java/com/fhdone/java2022/july/job/TaskQueryRunner.java
+++ july/src/main/java/com/fhdone/java2022/july/job/TaskQueryRunner.java
@@ -3,42 +3,68 @@ package com.fhdone.java2022.july.job;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.RandomStringUtils;
+import org.apache.logging.log4j.core.util.CronExpression;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.util.Random;
+import java.util.Date;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
-
+/**
+ * query job then offer into JOB_QUEUE[BlockingQueue]
+ */
 @Component
 @EnableScheduling
 @Slf4j
 public class TaskQueryRunner {
 
-    private static final int MAX_QUEUE_SIZE = 10;
+    private static final int MAX_QUEUE_SIZE = 4;
+    private static final String MOCK_JOB_CRON = "0/5 * * * * ?";
+    private static final int GEN_JOB_BATCH_SIZE = 2;
+    
+    private Date nextFireTime = new Date();
 
     @Getter
-    private static BlockingQueue<Object> JOB_QUEUE = new ArrayBlockingQueue<Object>(MAX_QUEUE_SIZE);
-
+    private static BlockingQueue<Object> JOB_QUEUE = new ArrayBlockingQueue<>(MAX_QUEUE_SIZE);
 
     @Scheduled(cron = "0/1 * * * * ?")
     public void taskQueryRunner() {
         log.info("taskQueryRunner start");
-        taskQuery();
+        try {
+            taskQuery();
+        }catch (Exception e){
+            log.error("taskQueryRunner error", e);
+        }
     }
 
-    private void taskQuery(){
-        for(int i=0; i<5; i++) {
-            String jobString = RandomStringUtils.random(20, true, true);
-            boolean r = JOB_QUEUE.offer(jobString);
-            if(!r){
-                log.warn("pub job {} error, ignore", jobString);
-                break;
-            }else{
-                log.info("success put job: {}" , jobString);
+    /**
+     * offer into JOB_QUEUE
+     */
+    private void taskQuery() throws Exception{
+
+        CronExpression cronExpression = new CronExpression(MOCK_JOB_CRON);
+        if(this.nextFireTime.before(new Date())){
+            log.debug("nextFireTime: {} , now:{}", nextFireTime, new Date());
+            this.nextFireTime = cronExpression.getTimeAfter(this.nextFireTime);
+            
+            for(int i=0; i<GEN_JOB_BATCH_SIZE; i++) {
+                String jobString = RandomStringUtils.random(20, true, true);
+                if(JOB_QUEUE.size() >= MAX_QUEUE_SIZE){
+                    log.info("JOB_QUEUE is full, ignore");
+                    continue;
+                }
+                boolean r = JOB_QUEUE.offer(jobString);
+                if(!r){
+                    log.warn("pub job {} error, ignore", jobString);
+                    break;
+                }else{
+                    log.info("success put job: {}" , jobString);
+                }
             }
+        }else{
+            log.info("nextFireTime not up to time");
         }
     }
 
