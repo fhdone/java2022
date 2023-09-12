d180be762a78977174ffcf4f94bf601f3ec52c1e
meteorafhd@gmail.com <meteorafhd@gmail.com>
2023-09-12 17:13:31 +0800

    try TransmittableThreadLocal

--- august/src/test/java/com/fhdone/java2022/august/utils/ThreadLocalDemo.java
+++ august/src/test/java/com/fhdone/java2022/august/utils/ThreadLocalDemo.java
@@ -6,11 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
-import java.util.function.BiConsumer;
-import java.util.function.Function;
 
 
 @Slf4j
@@ -19,46 +16,50 @@ public class ThreadLocalDemo {
 //     private static ThreadLocal<String> ttl = new ThreadLocal<>();
 //      private static ThreadLocal<String> ttl = new InheritableThreadLocal<>();
 
-    private static ThreadLocal<String> ttl = new TransmittableThreadLocal<>();
+    private static ThreadLocal<String> transmittableThreadLocal = new TransmittableThreadLocal<>();
 
-
-    /** 保证只有1个线程,以便观察这个线程被多个Runnable复用时，能否成功完成ThreadLocal的传递 **/
-    private static ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
-            1,1,0,TimeUnit.SECONDS,new ArrayBlockingQueue<>(10)
-    );
 
     @Test
     public void test() throws Exception {
-        ttl.set("raw info");
+
+        /** 保证只有1个线程,以便观察这个线程被多个Runnable复用时，能否成功完成ThreadLocal的传递 **/
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
+                1,1,0,TimeUnit.SECONDS,new ArrayBlockingQueue<>(10)
+        );
+
+        transmittableThreadLocal.set("raw info");
 
         for (int i = 0; i < 5; i++) {
             if (i == 2) {
-                ttl.set("raw info has changed");
+                transmittableThreadLocal.set("raw info has changed");
             }
             TtlRunnable runnable = TtlRunnable.get(() -> {
-                log.info(Thread.currentThread().getName() + " : " + ttl.get());
+                log.info(Thread.currentThread().getName() + " : " + transmittableThreadLocal.get());
             });
 
             threadPoolExecutor.execute(runnable);
 
-            CompletableFuture<Void> future = CompletableFuture.runAsync(runnable,threadPoolExecutor);
-            future.whenComplete(new BiConsumer<Void, Throwable>() {
-                @Override
-                public void accept(Void t, Throwable action) {
-                    log.info("执行完成！");
-                }
-
-            });
-            future.exceptionally(new Function<Throwable, Void>() {
-                @Override
-                public Void apply(Throwable t) {
-                    log.info("执行失败！"+t.getMessage());
-                    return null;
-                }
-            });
+//            CompletableFuture<Void> future = CompletableFuture.runAsync(runnable,threadPoolExecutor);
+//            future.whenComplete(new BiConsumer<Void, Throwable>() {
+//                @Override
+//                public void accept(Void t, Throwable action) {
+//                    log.info("执行完成！");
+//                }
+//
+//            });
+//            future.exceptionally(new Function<Throwable, Void>() {
+//                @Override
+//                public Void apply(Throwable t) {
+//                    log.info("执行失败！"+t.getMessage());
+//                    return null;
+//                }
+//            });
 
             TimeUnit.MILLISECONDS.sleep(500);
         }
     }
+
+
+
 
 }
