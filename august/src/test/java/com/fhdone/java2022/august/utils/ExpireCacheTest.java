199bb1a9cd822e3eea495da701cca595e36663e4
lewei <meteorafhd@gmail.com>
2024-09-08 10:16:21 +0800

    update

--- august/src/test/java/com/fhdone/java2022/august/utils/ExpireCacheTest.java
+++ august/src/test/java/com/fhdone/java2022/august/utils/ExpireCacheTest.java
@@ -1,5 +1,8 @@
 package com.fhdone.java2022.august.utils;
 
+import com.alibaba.fastjson.JSON;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
@@ -48,25 +51,45 @@ public class ExpireCacheTest {
     @Test
     public void testExpire2() throws Exception {
 
-        ExpireCache<String> cache = new ExpireCache<>(5, TimeUnit.SECONDS);
-        ExpireCache<Map<String, String>> mapCache = new ExpireCache<>(5, TimeUnit.SECONDS);
+//        ExpireCache<String> cache = new ExpireCache<>(5, TimeUnit.SECONDS);
+        Cache<String, String> cache =  CacheBuilder.newBuilder()
+                .maximumSize(100)
+                .expireAfterWrite(1, TimeUnit.SECONDS)
+                .removalListener(notification -> log.info("notification={}", JSON.toJSONString(notification)))
+                .build();
+
+
+//        ExpireCache<Map<String, String>> mapCache = new ExpireCache<>(5, TimeUnit.SECONDS);
+        Cache<String, Map<String, String>> mapCache =  CacheBuilder.newBuilder()
+                .maximumSize(100)
+                //配置写入后多久使缓存过期-下文会讲述
+                .expireAfterWrite(1, TimeUnit.SECONDS)
+                //配置写入后多久刷新缓存-下文会讲述
+                //.refreshAfterWrite(1, TimeUnit.SECONDS)
+                //key使用弱引用-WeakReference
+                //.weakKeys()
+                //当Entry被移除时的监听器
+                .removalListener(notification -> log.info("notification={}", JSON.toJSONString(notification)))
+                .build();
 
         Map<String, String> mapInfo = new HashMap<>();
         mapInfo.put("mapKey", "mapValue");
 
-        cache.put("testKey", "testValue", 1500);
-        mapCache.put("testMap", mapInfo, 1500);
+        cache.put("testKey", "testValue");
+//        cache.invalidateAll();
+        mapCache.put("testMap", mapInfo);
 
-        assertEquals("testValue", cache.get("testKey"));
-        assertEquals("mapValue", mapCache.get("testMap").get("mapKey"));
+        assertEquals("testValue", cache.getIfPresent("testKey"));
+        assertEquals("mapValue", mapCache.getIfPresent("testMap").get("mapKey"));
 
 
         new Thread(() -> {
-            Map<String, String> mapV = mapCache.get("testMap");
+            Map<String, String> mapV = mapCache.getIfPresent("testMap");
             for(int i=0; i<20; i++) {
                 log.info("count:{}", i);
                 assertEquals("mapValue", mapV.get("mapKey"));
                 log.info(mapV.get("mapKey"));
+                log.info(JSON.toJSONString(mapCache.getIfPresent("testMap")));
                 try {
                     TimeUnit.SECONDS.sleep(1);
                 } catch (InterruptedException e) {
@@ -80,8 +103,8 @@ public class ExpireCacheTest {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        assertNull(cache.get("test"));
-        
+        assertNull(cache.getIfPresent("test"));
+
         TimeUnit.SECONDS.sleep(30);
     }
 
